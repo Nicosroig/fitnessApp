7ded71b5215c45f542665bd8ef90abe5eafafc06
Nicolas Salomone <nicosroig@gmail.com>
2020-09-23 20:57:48 -0300

    Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// const element = document.createElement("h1");
+// element.innerText = "Hello React";
+// const container = document.getElementById("root");
+// container.appendChild(element);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./components/Card";
+import "bootstrap/dist/css/bootstrap.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const container = document.getElementById("root");
+
+//ReactDOM.render(__QUE__, __DONDE__);
+ReactDOM.render(<Card />, container);
