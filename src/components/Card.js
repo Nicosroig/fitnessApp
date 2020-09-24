4debda40fb0a29dbd0cda5add68c1964424cb0fa
Nicolas Salomone <nicosroig@gmail.com>
2020-09-24 20:41:34 -0300

    Commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,45 @@
 import React, { Component } from "react";
-import excerciseImg from "../images/exercise.png";
+import circlesImg from "../images/circles.png";
 import "./css/Card.css";
 
 class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: "http://lorempixel.com/200//200",
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06",
+      });
+    }, 5000);
+  }
+
   render() {
+    const { title, description, img, leftColor, rightColor } = this.props;
     return (
-      <div className="card mx-auto Fitness-Card">
+      <div
+        className="card mx-auto Fitness-Card"
+        style={{
+          backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`,
+        }}
+      >
         <div className="card-body">
           <div className="row center">
             <div className="col-6">
               <img
-                src={excerciseImg}
+                src={this.state.image}
                 alt="Guy doing exercise"
                 className="float-right"
               />
             </div>
             <div className="col-6 Fitness-Card-Info">
-              <h1>Technique Guides</h1>
-              <p>Learn amazing street workout and calisthenics</p>
+              <h1>{title}</h1>
+              <p>{description}</p>
             </div>
           </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-// const element = document.createElement("h1");
-// element.innerText = "Hello React";
-// const container = document.getElementById("root");
-// container.appendChild(element);
-
 import React from "react";
 import ReactDOM from "react-dom";
-import Card from "./components/Card";
+import Exercises from "./pages/Exercises";
 import "bootstrap/dist/css/bootstrap.css";
 
 const container = document.getElementById("root");
 
 //ReactDOM.render(__QUE__, __DONDE__);
-ReactDOM.render(<Card />, container);
+ReactDOM.render(<Exercises />, container);
