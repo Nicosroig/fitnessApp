ebaf785e0c88e1956008a997370543aafa6098d2
Nicolas Salomone <nicosroig@gmail.com>
2020-09-24 21:26:07 -0300

    Commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,22 +3,6 @@ import circlesImg from "../images/circles.png";
 import "./css/Card.css";
 
 class Card extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      image: "http://lorempixel.com/200//200",
-    };
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        image:
-          "https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06",
-      });
-    }, 5000);
-  }
-
   render() {
     const { title, description, img, leftColor, rightColor } = this.props;
     return (
@@ -31,11 +15,7 @@ class Card extends Component {
         <div className="card-body">
           <div className="row center">
             <div className="col-6">
-              <img
-                src={this.state.image}
-                alt="Guy doing exercise"
-                className="float-right"
-              />
+              <img src={img} alt="Guy doing exercise" className="float-right" />
             </div>
             <div className="col-6 Fitness-Card-Info">
               <h1>{title}</h1>
